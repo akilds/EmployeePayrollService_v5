aa54b20cec4109e38b13f7e6b4e9c014650f2206
akilds <akilrainadesigan@gmail.com>
2021-05-21 10:29:08 +0530

    ADDED : Lambok Library

--- src/main/java/com/example/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/ResponseDTO.java
@@ -1,27 +1,13 @@
 package com.example.employeepayrollapp.dto;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public @Data class ResponseDTO {
 	private String message;
 	private Object data;
 	
 	public ResponseDTO(String message, Object data) {
 		this.message = message;
-		this.data = data;
-	}
-	
-	public String getMessage() {
-		return message;
-	}
-	
-	public Object getData() {
-		return data;
-	}
-	
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	public void setData(Object data) {
 		this.data = data;
 	}
 }
