319b384fdd00ffc4ec2d50decf4c9ce19de94db6
akilds <akilrainadesigan@gmail.com>
2021-05-21 17:22:27 +0530

    ADDED : Setting Database

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,6 +2,7 @@ package com.example.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
-//		log.info("Employee Payroll App Started");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				  context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB user is {} ",
+				  context.getEnvironment().getProperty("spring.datasource.username"));;
 	}
-
 }
