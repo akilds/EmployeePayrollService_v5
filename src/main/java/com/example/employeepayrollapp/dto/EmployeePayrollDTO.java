a26053b53120295ea529cd04ecb271d374904260
akilds <akilrainadesigan@gmail.com>
2021-05-22 08:31:21 +0530

    ADDED : Name Validation

--- src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.example.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+	
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name invalid")
 	public String name;
+
+	@Min(value = 500, message = "Minimum wage should be more than 500")
 	public long salary;
 	
 	public EmployeePayrollDTO(String name, long salary) {
