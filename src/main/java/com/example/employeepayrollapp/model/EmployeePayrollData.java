c674d97d601086647c8bc80887f39398fe958792
akilds <akilrainadesigan@gmail.com>
2021-05-22 12:44:42 +0530

    ADDED : Saving in Database

--- src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/example/employeepayrollapp/model/EmployeePayrollData.java
@@ -3,25 +3,44 @@ package com.example.employeepayrollapp.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int employeeId;
+	
+	@Column(name = "name")
 	private String name;
 	private long salary;
 	private String gender;
 	private LocalDate startDate;
 	private String note;
 	private String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
 	public List<String> departments;
 	
 	public EmployeePayrollData() {}
 	
-	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
-		this.employeeId = empId;
+	public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		this.updateEmployeePayrollData(empPayrollDTO);
 	}
 
@@ -33,7 +52,5 @@ public @Data class EmployeePayrollData {
 		this.startDate = empPayrollDTO.startDate;
 		this.profilePic = empPayrollDTO.profilePic;
 		this.departments = empPayrollDTO.departments;
-	}
-	
-	
+	}	
 }
